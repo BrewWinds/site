e4839e5628c66d1babe4a14ecae8f83477121ccc
brew_winds <2282355136@qq.com>
2015-11-17 18:35:25 +0800

    domain module

--- minor/minor-domain/src/main/java/com/minor/domain/App.java
+++ minor/minor-domain/src/main/java/com/minor/domain/App.java
@@ -1,4 +1,4 @@
-package org.minor.service;
+package com.minor.domain;
 
 /**
  * Hello world!
---
+++ minor/minor-service/src/main/java/com/minor/service/App.java
@@ -0,0 +1,14 @@
+package com.minor.service;
+
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
